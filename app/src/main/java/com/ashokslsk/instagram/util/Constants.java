0e357a92d7f4911742d774c5e8e021186eaeb83d
ashok.kumar <ashok.kumar@dunstconsulting.in>
2016-02-08 15:00:17 +0530

    Instagram API integration

--- app/src/main/java/com/ashokslsk/instagram/util/Constants.java
+++ app/src/main/java/com/ashokslsk/instagram/util/Constants.java
@@ -5,8 +5,8 @@ package com.ashokslsk.instagram.util;
  */
 public class Constants {
 
-    public static final String CLIENT_ID = "103babf92fe1445f8bc4a38a8e2cfa58";
-    public static final String CLIENT_SECRET = "a1b7991ed1dc43df9ba8e706d4296b23";
+    public static final String CLIENT_ID = "YOUR_CLIENT_KEY";
+    public static final String CLIENT_SECRET = "YOUR_CLEINT_SECRET";
     public static final String CALLBACK_URL = "instagram://connect";
 
 
